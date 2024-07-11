b292d2fc915144790fcd20a33d046c6bef609497
XJloTT4eH9l <andriy.reka@sporthub.tech>
2024-07-12 01:07:45 +0300

    added getBreed function to the apiRequestStore and refactor BreedsPage

--- src/pages/BreedsPage.vue
+++ src/pages/BreedsPage.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
     import { ref, onMounted } from 'vue';
+    import { useApiRequests } from '@/stores/apiRequests';
 
     import HeaderPanel from '@/components/HeaderPanel.vue';
     import BackButton from '@/components/BackButton.vue';
@@ -8,72 +9,38 @@
     import Select from '@/components/Select.vue';
     import Loader from '@/components/Loader.vue';
 
-    import { BREEDS } from '@/catApi';
     import type { BreedItemShort } from '@/types';
     import type { Ref } from 'vue';
 
+    const apiRequests = useApiRequests();
     const breedsList: Ref<BreedItemShort[]> = ref([]);
-    const isLoading: Ref<boolean> = ref(false);
-    const limitOptions: string[] = ['All', '5', '10', '15', '20'];
     const sorting: Ref<string> = ref('down');
+    const limitOptions: string[] = ['All', '5', '10', '15', '20'];
+
+    const getBreedsHandler = (params?: string): void => {
+        apiRequests.getBreeds(sorting.value, params).then(res => {
+            if(!res) return
+            breedsList.value = res;
+        });
+    }
 
     const onLimitChanged = (option: string): void => {
         if(option === 'All') {
-            getBreeds();
+            getBreedsHandler();
             return
         }
 
         const searchParams = new URLSearchParams({ limit: option });
-        getBreeds(searchParams.toString());
-    }
-
-    const sortBreeds = (breeds: BreedItemShort[], direction: string): BreedItemShort[] => {
-        return breeds.sort((a, b) => {
-            const comparison = a.name.localeCompare(b.name);
-            return direction === 'down' ? comparison : -comparison;
-        });
+        getBreedsHandler(searchParams.toString());
     }
 
     const sortingHandler = (sortOption: string): void => {
         sorting.value = sortOption;
-        if(sorting.value === 'down') {
-            sortBreeds(breedsList.value, 'down');
-        } else {
-            sortBreeds(breedsList.value, 'up');
-        }
-    }
-
-    const getBreeds = async (params?: string): Promise<void> => {
-        try {
-            isLoading.value = true;
-            let url = BREEDS;
-
-            if(params) {    
-                url += `&${params}`;
-            }
-
-            const responce = await fetch(url);
-
-            if(!responce.ok) {
-                throw new Error('Cant get data')
-            }
-
-            const responceJson = await responce.json();
-
-            if(sorting.value === 'down') {
-                breedsList.value = sortBreeds(responceJson, 'down');
-            } else {
-                breedsList.value = sortBreeds(responceJson, 'up');
-            }
-        } catch (error) {
-            console.log(error);
-        } finally {
-            isLoading.value = false;
-        }
+        getBreedsHandler();
     }
 
     onMounted(() => {
-        getBreeds()
+        getBreedsHandler();
     })
 </script>
 
@@ -103,7 +70,7 @@
                 />
             </div>
         </div>
-        <Loader v-if="isLoading"/>
+        <Loader v-if="apiRequests.isLoading" />
         <div class="cats-list" v-else-if="breedsList.length > 0">
             <BreedItem 
                 v-for="item in breedsList"
--- src/stores/apiRequests.ts
+++ src/stores/apiRequests.ts
@@ -1,40 +1,64 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
 import type { Ref } from 'vue';
-import { VOTING_IMG } from '@/catApi';
-import type { CatObject } from '@/types';
+import { VOTING_IMG, BREEDS } from '@/catApi';
+import type { CatObject, BreedItemShort } from '@/types';
 
 export const useApiRequests = defineStore('apiRequests', () => {
     const isLoading:Ref<boolean> = ref(false);
     const isError:Ref<boolean> = ref(false);
 
-    const fetchHelper = async (url: string): Promise<any | null> => {
+    const fetchHelper = async (url: string, params?: string, sort?: string): Promise<any> => {
         try {
             isLoading.value = true;
-            const response = await fetch(url);
+            let fetchUrl: string = url;
+
+            if(params) {
+                fetchUrl += `&${params}`;
+            }
+
+            const response = await fetch(fetchUrl);
 
             if(!response.ok) {
                 throw new Error('Cant get data')
             } else {
                 isError.value = false;
             }
+            
+            const responceJson =  await response.json();
 
-            return await response.json();
+            if(sort) {
+                return sortBreeds(responceJson, sort);
+            }
+
+            return responceJson
         } catch (error) {
             isError.value = true;
             return null
         } finally {
             isLoading.value = false;
         }
-    }
+    };
 
     const getVote = async (): Promise<CatObject[] | null> => {
-        return fetchHelper(VOTING_IMG);
+        return await fetchHelper(VOTING_IMG);
+    };
+
+    const getBreeds = async (sort: string, params?: string, ): Promise<BreedItemShort[] | null> => {
+        return await fetchHelper(BREEDS, params, sort);
+    };
+
+    const sortBreeds = (breeds: BreedItemShort[], direction: string): BreedItemShort[] => {
+        return breeds.sort((a, b) => {
+            const comparison = a.name.localeCompare(b.name);
+            return direction === 'down' ? comparison : -comparison;
+        });
     }
 
     return {
         isLoading,
         isError,
-        getVote
+        getVote,
+        getBreeds
     }
 });
